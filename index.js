d3cf30306b65a17943a708cc278f732b030a27c4
oweingart <oweingart@forter.com>
2019-02-25 17:03:11 +0200

    added multiple demos

--- index.js
+++ index.js
@@ -16,80 +16,142 @@ const get = (obj, path) => {
 const SECOND = 1000;
 
 /**
- * TODO
- *
+ * Mixing that run lit-element's requestUpdate() when a prop is changed on the object
+ * @param obj
+ * @param elm
+ */
+const objectWatcherMixing = (obj, elm, scope) => {
+    obj.__litElms = obj.__litElms || [];
+    obj.__litElms.push(elm);
+    Object.keys(obj).filter(key=>!key.startsWith('__')).map(
+      prop => {
+          let value = obj[prop];
+          delete obj[prop];
+          obj[`__${prop}`] = value;
+          Object.defineProperty(obj, prop, {
+              get: function () {
+                  return obj[`__${prop}`]
+              },
+              set: function (val) {
+                  obj[`__${prop}`] = val;
+                  obj.__litElms.map(
+                    theElm => theElm.requestUpdate()
+                  );
+                  scope.$applyAsync();
+              }
+          });
+      }
+    )
+}
+
+/**
+ * Mixing that run lit-element's requestUpdate() when an array was changed
+ * @param arr
+ * @param elm
+ */
+const ARRAY_MUTATIONS = ['fill', 'pop', 'push', 'reverse', 'shift', 'sort', 'splice', 'unshift'];
+const arrayWatcherMixing = (arr, elm, scope) => {
+    ARRAY_MUTATIONS.map(
+      mutationName => {
+          arr.__litElms = arr.__litElms || [];
+          arr.__litElms.push(elm);
+          arr[mutationName] = function () {
+              Array.prototype[mutationName].apply(this, arguments);
+              arr.__litElms.map(
+                theElm => theElm.requestUpdate()
+              );
+              scope.$applyAsync();
+          }
+      }
+  );
+};
+
+/**
  * @param {LitElement} baseElement - the LitElement to extend
  */
-export const NgLit = (baseElement) => class extends baseElement {
-
+export const NgLit = baseElement => class extends baseElement {
     /**
      * Extend the LitElement `createProperty` method to map angular properties
      */
     static createProperty(name, options) {
         super.createProperty(name, options);
-
         if (options.fromNg) {
-            if (!this._ngProp) {
-                this._ngProp = {}
+            if (!this._ngProperties) {
+                this._ngProperties = {}
             }
-
-            this._ngProp[name] = options;
+            this._ngProperties[name] = options;
         }
     }
-
     /**
-     * check for changed properties and inject anuglar properties if possible
+     * Runs a callback on the next digest cycle
+     * @param cb
      */
-
-
+     _nextDigestWithScope (cb, waitTime) {
+        const scope = angular.element(this.parentElement).scope();
+        if (scope) {
+            // Try to extract angular's $apply, otherwise use setTimeout
+            const $body = angular.element(
+              document.getElementsByTagName('ng-app')[0] ||
+              document.querySelector("[ng-app]")
+            );
+            const nextDigest = get($body, 'scope().$root.$apply') || setTimeout;
+            nextDigest(()=>{
+                cb({scope})
+            })
+        } else {
+            if (waitTime) {
+                setTimeout(()=>{
+                    this._nextDigestWithScope(cb,0)
+                }, waitTime)
+            }
+        }
+    }
+    /**
+     * Check for changed properties and inject angular properties from scope if possible
+     */
     update(changedProps) {
-
-        if (!this.constructor._ngProp) {
+        if (!this.constructor._ngProperties) {
             super.update(changedProps);
             return;
         }
+        this._nextDigestWithScope(({scope})=> {
+            this._commitWIthScope({scope, changedProps})
+        }, SECOND * 0.01)
+    }
 
-        for (let [name] of Object.entries(this.constructor._ngProp)) {
-            const ngPropRawValue = this.getAttribute(name);
-            if (ngPropRawValue) {
-                const $body = angular.element(
-                    document.getElementsByTagName('ng-app')[0] ||
-                    document.querySelector("[ng-app]"));
-                const nextDigest = get($body, 'scope().$root.$apply') || setTimeout;
-                nextDigest(() => {
-                    let scope;
-                    if (scope) {
-                        // TODO: validation
-                        this._commitWIthScope({scope, elm: this, changedProps, ngPropRawValue, name})
-                    } else {
-                        nextDigest(() => {
-                            scope = angular.element(this.parentElement).scope();
-                            if (!scope) {
-                                setTimeout(() => {
-                                    scope = angular.element(this.parentElement).scope();
-                                    if (!scope) {
-                                        return;
-                                    }
-                                    this._commitWIthScope({scope, elm: this, changedProps, ngPropRawValue, name});
-                                }, SECOND * 0.3)
-                            } else {
-                                this._commitWIthScope({scope, elm: this, changedProps, ngPropRawValue, name});
-                            }
-                        });
+    /**
+     * Commit an update with the scope and the changedProp
+     * @param scope
+     * @param changedProps
+     * @private
+     */
+    _commitWIthScope ({scope, changedProps}) {
+        for (let [ngPropName, ngPropOptions] of Object.entries(this.constructor._ngProperties)) {
+            const ngPropRawValue = this.getAttribute(ngPropName);
+            let ngValue = get(scope, ngPropRawValue);
+            if (!ngValue || typeof ngValue == 'string') {
+                this[ngPropName] = null;
+                changedProps.delete(ngPropName);
+            } else {
+                // Watch it
+                if (ngPropOptions.watch && !ngPropOptions._watcher) {
+                    // Array
+                    if (Array.isArray(ngValue)) {
+                        ngPropOptions._watcher = true;
+                        arrayWatcherMixing(ngValue, this, scope);
                     }
-                });
+                    // Object
+                    if (ngValue.constructor == Object) {
+                        ngPropOptions._watcher = true;
+                        objectWatcherMixing(ngValue, this, scope)
+                    }
+                }
+                this[ngPropName] = ngValue;
+                changedProps.set(ngPropName, ngValue);
             }
         }
-
-    }
-
-    _commitWIthScope ({scope, changedProps, ngPropRawValue, name}) {
-        let ngValue = get(scope, ngPropRawValue);
-        changedProps.set(name, ngValue);
-        this[name] = ngValue;
         super.update(changedProps);
     }
-
 };
 
 export default NgLit;
