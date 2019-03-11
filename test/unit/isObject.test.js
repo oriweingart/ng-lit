31da58c930d86d55987b33cc2bcf095804f594a4
Ariel Gorlik <41146054+gorlikariel@users.noreply.github.com>
2019-03-11 16:29:56 +0200

    Update isObject.test.js

--- test/unit/isObject.test.js
+++ test/unit/isObject.test.js
@@ -3,7 +3,7 @@ import assert from 'assert';
 import { describe } from "mocha";
 import { isObject } from "../../watchers";
 
-describe('isObect', () => {
+describe('isOjbect', () => {
 
   it('should return true for object', () => {
     assert(isObject({}) === true);
